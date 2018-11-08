81431578e87fb5328933e16ee385992ea4df4db9
reil8450 <reil8450@ISS2TYPKH2.scdsb.pri>
2018-11-08 14:32:13 -0500

    Added new Search Example

---
+++ src/Searching/State.java
@@ -0,0 +1,19 @@
+package Searching;
+
+public class State implements Comparable <State>{
+    
+    private String name;
+    
+    public State(String n){
+        name = n;
+    }
+    
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public int compareTo(State otherState) {
+        return name.compareTo(otherState.getName());
+    }
+}
